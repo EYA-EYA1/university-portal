212aa7e1495d92957efd6f0992d79ea3fa16407d
eya <eyahamdaoui4@gmail.com>
2026-09-24 12:49:09 +0100

    feat: add departments page with database data

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from run import db
+from extensions import db
 
 class Department(db.Model):
     id = db.Column(db.Integer, primary_key=True)
--- run.py
+++ run.py
@@ -1,16 +1,21 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from extensions import db
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///portal.db"
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
 from models import Department
 
 @app.route("/")
 def home():
     return render_template("home.html", name="Eya")
+
+@app.route("/departments")
+def departments():
+    all_departments = Department.query.all()
+    return render_template("departments.html", departments=all_departments)
 
 if __name__ == "__main__":
     app.run(debug=True)
